84b3011e02dd8b859231c738f732f8b8e715a015
Ken <bassettkennethj@gmail.com>
2023-01-08 08:58:44 -0500

    Zacks converted. Code never tested.

--- Sources/zacks.py
+++ Sources/zacks.py
@@ -1,10 +1,12 @@
 from bs4 import BeautifulSoup
 import numpy as np
+import pandas as pd
+from selenium import webdriver
 
 
-def _zacks(row, driver):
+def get_rating(driver, ticker):
     # -1, -0.5, 0, 0.5, 1
-    driver.get("http://www.zacks.com/stock/quote/" + row['Ticker'])
+    driver.get("http://www.zacks.com/stock/quote/" + ticker)
     soup = BeautifulSoup(driver.page_source, "html.parser")
     try:
         rec = int(soup.find('p', attrs={'class': 'rank_view'}).text.strip()[0])
@@ -14,5 +16,43 @@ def _zacks(row, driver):
     except AttributeError:
         rec = np.nan
 
-    row['Zacks'] = rec
-    return row
+    return rec
+
+
+def main(send_q, recv_q):
+    driver = webdriver.Chrome()
+    tickers = pd.Series()
+    while True:
+
+        for i in tickers.index:
+            t = tickers.pop(i)
+            print(f'Zacks\t{t}\t{len(tickers)}')
+            send_q.put({'Ticker': t, 'Zacks': get_rating(driver, t)})
+
+        if not recv_q.empty():
+            r = recv_q.get()
+            if isinstance(r, pd.Series):  # More tickers
+                tickers = pd.concat([tickers, r])
+            elif isinstance(r, str):
+                if r == 'STOP':
+                    break
+
+    driver.quit()
+
+
+if __name__ == '__main__':
+    from timeit import default_timer as timer
+    from datetime import timedelta
+    from multiprocessing import Queue, Process
+
+    start = timer()
+    q1 = Queue()
+    q2 = Queue()
+    p = Process(target=main, args=(q1, q2))
+    p.start()
+    msg = None
+    while not isinstance(msg, str):
+        msg = q1.get()
+        print(msg)
+    stop = timer()
+    print(timedelta(seconds=stop - start))
