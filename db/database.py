d9dc7f45e2009de123e454af3dc530cb50b78c26
Ken <bassettkennethj@gmail.com>
2023-11-01 22:41:18 -0400

    Database convenience upgrade

--- db/database.py
+++ db/database.py
@@ -1,38 +1,97 @@
 import os
 import sqlite3
 from config import CONFIG
-from typing import List, Tuple
+from typing import List, Tuple, Union, Dict, Any
+import pandas as pd
 
 
 class Database:
     def __init__(self, db_path: str):
         self.db_path = db_path
 
+    def __call__(
+            self,
+            query: str,
+            params: Tuple = (),
+            return_type: str = 'list'
+    ) -> Union[pd.DataFrame, List[Tuple]]:
+        """Make the Database object callable to execute queries."""
+        return self.execute_query(query, params, return_type)
+
     def _connect(self):
         """Create a database connection."""
         return sqlite3.connect(self.db_path)
 
-    def execute_query(self, query: str, params: Tuple = ()):
-        """Execute a single SQL query."""
+    def execute_query(
+            self,
+            query: str,
+            params: Union[Tuple, List] = (),
+            return_type: str = 'list'
+    ) -> Union[pd.DataFrame, List[Tuple]]:
+        """
+        Execute a SQL query with the specified return type.
+        Emphasis on convenience.
+
+        Args:
+            query (str): The SQL query to execute.
+            params (Union[Tuple, List], optional): Parameters to substitute in the query.
+            return_type (str, optional): The desired return type ('DataFrame' or 'List').
+
+        Returns:
+            Union[pd.DataFrame, List[Tuple]]: The query results in the specified return type.
+
+        If `return_type` is set to 'DataFrame', the results are returned as a Pandas DataFrame.
+        If `return_type` is set to 'list', the results are returned as a list of tuples.
+
+        The function can handle both single query executions and bulk insert operations based on the data type
+        of `params`. When `params` is a list of tuples, it performs an `executemany` operation.
+        """
         with self._connect() as conn:
             cursor = conn.cursor()
-            cursor.execute(query, params)
+
+            if return_type == 'DataFrame':
+                cursor.execute(query, params)
+                # Use Pandas to read the results into a DataFrame
+                result = pd.read_sql_query(query, conn, params=params)
+            elif return_type == 'list':
+                # Check if params is a list of tuples for executemany
+                if isinstance(params, list) and all(isinstance(p, tuple) for p in params):
+                    cursor.executemany(query, params)
+                    result = None  # No direct result for executemany
+                else:
+                    cursor.execute(query, params)
+                    result = cursor.fetchall()
+            else:
+                raise ValueError("Invalid return_type. Use 'DataFrame' or 'List'.")
+
             conn.commit()
-            return cursor.fetchall()
 
-    def insert(self, table: str, column_values: dict):
-        """Insert a record into the database."""
-        columns = ', '.join(column_values.keys())
-        placeholders = ', '.join(['?'] * len(column_values))
+        return result
+
+    def insert(self, table: str, data: Union[Dict[str, Any], pd.DataFrame]):
+        """Insert a record or records into the database."""
+        if isinstance(data, dict):
+            self._insert_single_record(table, data)
+        elif isinstance(data, pd.DataFrame):
+            self._insert_multiple_records(table, data)
+        else:
+            raise ValueError("Unsupported data type. Use a dictionary or a DataFrame.")
+
+    def _insert_single_record(self, table: str, data: Dict[str, Any]):
+        """Insert a single record into the database."""
+        columns = ', '.join(data.keys())
+        placeholders = ', '.join(['?'] * len(data))
         query = f"INSERT INTO {table} ({columns}) VALUES ({placeholders});"
-        self.execute_query(query, tuple(column_values.values()))
+        params = tuple(data.values())
+        self.execute_query(query, params)
 
-    def select(self, table: str, columns: List[str] = None):
-        """Select a record from the database."""
-        if columns is None:
-            columns = '*'
-        query = f"SELECT {columns} FROM {table};"
-        return self.execute_query(query)
+    def _insert_multiple_records(self, table: str, data: pd.DataFrame):
+        """Insert multiple records from a DataFrame into the database."""
+        columns = ', '.join(data.columns)
+        placeholders = ', '.join(['?'] * len(data.columns))
+        query = f"INSERT INTO {table} ({columns}) VALUES ({placeholders});"
+        params = [tuple(row) for row in data.values]
+        self.execute_query(query, params)
 
 
 def create_database_if_not_exists(db_path: str, schema_path: str = 'create_db.sql'):
@@ -54,4 +113,11 @@ def create_database_if_not_exists(db_path: str, schema_path: str = 'create_db.sq
 
 
 if __name__ == '__main__':
-    create_database_if_not_exists(CONFIG['db_folder'] + CONFIG['db_name'])
+    pass
+    # Examples
+    # name = CONFIG['db_folder'] + CONFIG['db_name']
+    # create_database_if_not_exists(name)
+    # db = Database(name)
+    # db.insert('Companies', {'name': 'Test Company2', 'symbol': 'TEST2'})
+    # db.insert('Companies', pd.DataFrame({'name': ['Test Company3', 'Test Company 4'], 'symbol': ['TEST3', 'TEST4']}))
+    # print(db('DELETE FROM Companies'))
