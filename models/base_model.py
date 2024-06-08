5601942c1ff1fb47c7361e264d25baeabf51f96c
Ken <bassettkennethj@gmail.com>
2024-06-08 17:08:56 -0400

    Created BaseModel for model layer

---
+++ models/base_model.py
@@ -0,0 +1,41 @@
+from typing import Any, Dict, List, Tuple, Union
+
+import pandas as pd
+from async_database import AsyncDatabase
+from async_lru import alru_cache
+
+
+class BaseModel:
+    def __init__(self, db: AsyncDatabase, table_name: str):
+        self.db = db
+        self.table_name = table_name
+
+    async def insert(self, data: Union[Dict[str, Any], pd.DataFrame, tuple]):
+        return await self.db.insert(self.table_name, data)
+
+    async def update(self, identifier: Any, data: Dict[str, Any]):
+        set_clause = ", ".join([f"{key} = ?" for key in data.keys()])
+        params = tuple(data.values()) + (identifier,)
+        query = f"UPDATE {self.table_name} SET {set_clause} WHERE id = ?;"
+        return await self.db.execute_query(query, params)
+
+    async def delete(self, identifier: Any):
+        query = f"DELETE FROM {self.table_name} WHERE id = ?;"
+        return await self.db.execute_query(query, (identifier,))
+
+    async def get(self, identifier: Any) -> Union[pd.DataFrame, List[Tuple]]:
+        query = f"SELECT * FROM {self.table_name} WHERE id = ?;"
+        return await self.db.execute_query(
+            query, (identifier,), return_type="DataFrame"
+        )
+
+    async def get_all(self) -> Union[pd.DataFrame, List[Tuple]]:
+        query = f"SELECT * FROM {self.table_name};"
+        return await self.db.execute_query(query, return_type="DataFrame")
+
+    @alru_cache
+    async def table_exists(self) -> bool:
+        result = await self.db.execute_query(
+            f"SELECT name FROM sqlite_master WHERE type = 'table' AND name = '{self.table_name}';"
+        )
+        return bool(result)
--- models/company.py
+++ models/company.py
@@ -1,28 +1,37 @@
 from async_lru import alru_cache
+from base_model import BaseModel
 
 
-@alru_cache(maxsize=500)
-async def get_or_create_company(
-    db, symbol: str = None, name: str = None, industry: str = None
-):
-    if not symbol and not name:
-        raise ValueError("Please provide either a symbol or a name.")
-    company = None
-    if symbol:
-        company = await db("SELECT * FROM Companies WHERE symbol =?", (symbol,))
-    elif name:
-        company = await db("SELECT * FROM Companies WHERE name =?", (name,))
+class Company(BaseModel):
+    def __init__(self, db):
+        super().__init__(db, "Companies")
 
-    # if not found, create a new company if symbol is provided
-    if not company:
+    @alru_cache(maxsize=500)
+    async def get_or_create_company(
+        self, symbol: str = None, name: str = None, industry: str = None
+    ):
+        if not symbol and not name:
+            raise ValueError("Please provide either a symbol or a name.")
+        company = None
         if symbol:
-            await db.insert(
-                "Companies", {"symbol": symbol, "name": name, "industry": industry}
+            company = await self.db(
+                "SELECT * FROM Companies WHERE symbol =?", (symbol,)
             )
-            company = await db("SELECT * FROM Companies WHERE symbol =?", (symbol,))
-        else:
-            raise ValueError(
-                "No company found, and no symbol provided to create new company."
-            )
+        elif name:
+            company = await self.db("SELECT * FROM Companies WHERE name =?", (name,))
 
-    return company[0]
+        # if not found, create a new company if symbol is provided
+        if not company:
+            if symbol:
+                await self.db.insert(
+                    "Companies", {"symbol": symbol, "name": name, "industry": industry}
+                )
+                company = await self.db(
+                    "SELECT * FROM Companies WHERE symbol =?", (symbol,)
+                )
+            else:
+                raise ValueError(
+                    "No company found, and no symbol provided to create new company."
+                )
+
+        return company[0]
