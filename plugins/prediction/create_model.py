5de4b18207707a2fa0fc6ba0491374db48e50fe7
Ken <bassettkennethj@gmail.com>
2024-06-30 14:11:51 -0400

    Removed testing code and parameterized some hard coded values

--- plugins/prediction/create_model.py
+++ plugins/prediction/create_model.py
@@ -1,6 +1,5 @@
-import numpy as np
 import tensorflow as tf
-from transformers import TFBertModel, BertTokenizer
+from transformers import TFBertModel
 
 
 # Define the text encoder using BERT
@@ -12,7 +11,13 @@ def get_text_embedding(text_model, input_ids, attention_mask):
 
 # Combine both encoders and define the final model
 def create_combined_model(
-    text_model_name, num_texts, structured_input_dim, combined_hidden_dim, output_dim
+    text_model_name,
+    num_texts,
+    structured_input_dim,
+    combined_hidden_dim,
+    output_dim,
+    output_activation="sigmoid",
+    dropout_rate=0.3,
 ):
     text_encoder = TFBertModel.from_pretrained(text_model_name)
 
@@ -51,90 +56,14 @@ def create_combined_model(
 
     # combined model is a fully connected neural network
     combined = tf.keras.layers.Dense(combined_hidden_dim, activation="relu")(combined)
-    combined = tf.keras.layers.Dropout(0.3)(combined)
+    combined = tf.keras.layers.Dropout(dropout_rate)(combined)
     combined = tf.keras.layers.Dense(combined_hidden_dim, activation="relu")(combined)
-    combined = tf.keras.layers.Dropout(0.3)(combined)
+    combined = tf.keras.layers.Dropout(dropout_rate)(combined)
 
-    output = tf.keras.layers.Dense(output_dim, activation="sigmoid")(combined)
+    output = tf.keras.layers.Dense(output_dim, activation=output_activation)(combined)
 
     model = tf.keras.Model(inputs=input_layer, outputs=output)
     model.summary()
     model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
 
     return model
-
-
-if __name__ == "__main__":
-    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-
-    # some fake data for testing
-    # assume we get 3 separate texts for each data point
-    texts = [
-        ["Buying this is a great idea."] * 3,
-        ["Selling this is a great idea."] * 3,
-        ["Buying this is a bad idea."] * 3,
-        ["Selling this is a bad idea."] * 3,
-        ["Buy this now."] * 3,
-        ["Sell this now."] * 3,
-        ["The price will increase."] * 3,
-        ["The price will decrease."] * 3,
-    ] * 300
-
-    all_inputs = []
-    buy = True
-    for text_group in texts:
-        group_inputs = np.array([])
-        for text in text_group:
-            tokenized_text = tokenizer(
-                text,
-                padding="max_length",
-                truncation=True,
-                max_length=512,
-                return_attention_mask=True,
-                return_tensors="tf",
-            )
-            input_ids = tokenized_text["input_ids"]
-            attention_mask = tokenized_text["attention_mask"]
-            group_inputs = np.append(group_inputs, input_ids)
-            group_inputs = np.append(group_inputs, attention_mask)
-        if buy:
-            group_inputs = np.append(group_inputs, np.ones(10))
-        else:
-            group_inputs = np.append(group_inputs, np.zeros(10))
-        buy = not buy
-        all_inputs.append(group_inputs)
-    # m x n
-    # n=(input_ids1 + attention_mask1 + input_ids2 + attention_mask2 + input_ids3 + attention_mask3 + structured_input)
-    all_inputs = np.array(all_inputs)
-
-    # Example labels
-    labels = np.array([1, 0] * int(len(texts) / 2))
-
-    # Create the combined model
-    combined_model = create_combined_model(
-        "bert-base-uncased",
-        len(texts[0]),
-        structured_input_dim=10,
-        combined_hidden_dim=128,
-        output_dim=1,
-    )
-
-    # Train the model with the sampled training dataset and validate with the sampled validation dataset
-    combined_model.fit(
-        all_inputs,
-        labels,
-        batch_size=1,
-        epochs=70,
-        steps_per_epoch=10,
-        shuffle=True,
-        validation_split=0.25,
-    )
-
-    # Make predictions
-    predictions = combined_model.predict([all_inputs])
-    print(predictions)
-
-    # add, commit, and push, ya ding dong
-    # 1. decide which data to get
-    # 2. get data
-    # 3. test
