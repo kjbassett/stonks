f872fb2d279308bb426c4d4562f879fc49fc3254
Ken <bassettkennethj@gmail.com>
2024-06-30 09:37:09 -0400

    added comments and removed some print statements

--- plugins/prediction/create_model.py
+++ plugins/prediction/create_model.py
@@ -1,6 +1,5 @@
 import numpy as np
 import tensorflow as tf
-from icecream import ic
 from transformers import TFBertModel, BertTokenizer
 
 
@@ -17,16 +16,18 @@ def create_combined_model(
 ):
     text_encoder = TFBertModel.from_pretrained(text_model_name)
 
+    # One input layer for all tokenized text inputs and structured numerical input
     input_layer = tf.keras.layers.Input(
         shape=(512 * 2 * num_texts + structured_input_dim,),
         dtype=tf.float32,
         name="combined_input",
     )
 
-    # variable number of text inputs
+    # for each text input, get corresponding slices from the input layer for the input ids and attention masks
     all_text_embeddings = []
     for i in range(num_texts):
         slice_start = i * 512 * 2
+        # first 512 tokens are input ids, next 512 are attention masks
         input_ids = tf.cast(
             input_layer[:, slice_start : slice_start + 512],
             dtype=tf.int32,
@@ -45,8 +46,10 @@ def create_combined_model(
         input_layer[:, -structured_input_dim:], name="structured_input"
     )
 
+    # combine all text embeddings and the structured input into a single input layer for the combined model
     combined = tf.keras.layers.Concatenate()([*all_text_embeddings, structured_input])
 
+    # combined model is a fully connected neural network
     combined = tf.keras.layers.Dense(combined_hidden_dim, activation="relu")(combined)
     combined = tf.keras.layers.Dropout(0.3)(combined)
     combined = tf.keras.layers.Dense(combined_hidden_dim, activation="relu")(combined)
@@ -65,6 +68,7 @@ if __name__ == "__main__":
     tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
     # some fake data for testing
+    # assume we get 3 separate texts for each data point
     texts = [
         ["Buying this is a great idea."] * 3,
         ["Selling this is a great idea."] * 3,
@@ -99,9 +103,9 @@ if __name__ == "__main__":
             group_inputs = np.append(group_inputs, np.zeros(10))
         buy = not buy
         all_inputs.append(group_inputs)
+    # m x n
+    # n=(input_ids1 + attention_mask1 + input_ids2 + attention_mask2 + input_ids3 + attention_mask3 + structured_input)
     all_inputs = np.array(all_inputs)
-    ic(all_inputs)
-    ic(all_inputs.shape)
 
     # Example labels
     labels = np.array([1, 0] * int(len(texts) / 2))
@@ -114,8 +118,6 @@ if __name__ == "__main__":
         combined_hidden_dim=128,
         output_dim=1,
     )
-
-    ic(combined_model.inputs)
 
     # Train the model with the sampled training dataset and validate with the sampled validation dataset
     combined_model.fit(
@@ -130,7 +132,7 @@ if __name__ == "__main__":
 
     # Make predictions
     predictions = combined_model.predict([all_inputs])
-    ic(predictions)
+    print(predictions)
 
     # add, commit, and push, ya ding dong
     # 1. decide which data to get
