03a85c17865d1c2e8fcbfbd17e74a1661594f33c
Ken <bassettkennethj@gmail.com>
2024-07-04 23:52:44 -0400

    Created new data access object for retrieving all the data necessary for training with one query. Removed obselete functions from other DAOs.

--- data_access/News.py
+++ data_access/News.py
@@ -22,21 +22,3 @@ class News(BaseDAO):
             query, (company_id, min_timestamp), return_type="DataFrame"
         )
         return data
-
-    async def get_n_most_recent_news(
-        self, company_id: int, n: int, min_timestamp: int = 0
-    ) -> pd.DataFrame:
-        query = f"""
-        SELECT *
-        FROM News INNER JOIN NewsCompanyLink
-        ON News.id = NewsCompanyLink.news_id
-        WHERE NewsCompanyLink.company_id =?
-        AND News.timestamp >?
-        ORDER BY News.timestamp DESC
-        LIMIT ?;
-        """
-
-        data = await self.db(
-            query, (company_id, min_timestamp, n), return_type="DataFrame"
-        )
-        return data
