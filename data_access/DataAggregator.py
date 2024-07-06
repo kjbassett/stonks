fc9df1661d946a80112494b8d7a6c486c663cd1b
Ken <bassettkennethj@gmail.com>
2024-07-05 22:37:26 -0400

    Added one-hot encoding for industry in DataAggregator

--- data_access/DataAggregator.py
+++ data_access/DataAggregator.py
@@ -23,7 +23,7 @@ class DataAggregator(BaseDAO):
         if windows is None:
             windows = [4, 19, 59, 389]
 
-        query = construct_query(
+        query = await self.construct_query(
             company_id,
             min_timestamp,
             max_timestamp,
@@ -46,118 +46,127 @@ class DataAggregator(BaseDAO):
 
         return data
 
+    async def construct_query(
+        self,
+        company_id,
+        min_timestamp,
+        max_timestamp,
+        avg_close,
+        avg_volume,
+        std_dev,
+        windows,
+        news_relative_age_threshold,
+        n_news,
+    ):
+        # Common table expressions
+        CTEs = []
+        columns = [
+            "t.company_id",
+            "t.timestamp",
+            "t.open",
+            "t.high",
+            "t.low",
+            "t.close",
+            "t.vw_average",
+            "t.volume",
+            "c.symbol",
+        ]
 
-def construct_query(
-    company_id,
-    min_timestamp,
-    max_timestamp,
-    avg_close,
-    avg_volume,
-    std_dev,
-    windows,
-    news_relative_age_threshold,
-    n_news,
-):
-    # Common table expressions
-    CTEs = []
-    columns = [
-        "t.company_id",
-        "t.timestamp",
-        "t.open",
-        "t.high",
-        "t.low",
-        "t.close",
-        "t.vw_average",
-        "t.volume",
-        "c.symbol",
-        "c.industry",
-    ]
-    # Get various statistics over each window
-    for window in windows:
-        if avg_close:
+        # one hot encode each industry
+        industries = await self.db.execute_query(
+            "SELECT * FROM Industry;", return_type="DataFrame"
+        )
+        for i, row in industries.iterrows():
             columns.append(
-                f"AVG(t.close) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {window} PRECEDING AND CURRENT ROW) AS ma_{window}"
-            )
-        if avg_volume:
-            columns.append(
-                f"AVG(t.volume) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {window} PRECEDING AND CURRENT ROW) AS avg_volume_{window}"
-            )
-        if std_dev:
-            columns.append(
-                f"""SQRT(AVG(t.close * t.close) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {window} PRECEDING AND CURRENT ROW) - 
-                    (AVG(t.close) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {window} PRECEDING AND CURRENT ROW) * 
-                    AVG(t.close) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {window} PRECEDING AND CURRENT ROW))) AS volatility_{window}"""
+                f"CASE WHEN (t.industry_id = {row.id}) THEN 1 ELSE 0 END AS industry_{row.name}"
             )
 
-    # get n_news columns of the last n news articles before TradingData.timestamp
-    ranked_news_joins = []
-    for idx in range(1, n_news + 1):
-        columns.append(f"n{idx}.news_id AS news{idx}_id")
-        columns.append(f"n{idx}.timestamp AS news{idx}_timestamp")
-        ranked_news_joins.append(
-            f"LEFT JOIN RankedNews n{idx} ON t.company_id = n{idx}.company_id AND t.timestamp = n{idx}.trading_timestamp AND n{idx}.rn = {idx}"
-        )
-    if ranked_news_joins:
-        CTEs.append(
-            f"""
-            RankedNews AS (
-                SELECT
-                    n.id AS news_id,
-                    n.timestamp,
-                    t.company_id,
-                    t.timestamp AS trading_timestamp,
-                    ROW_NUMBER() OVER (PARTITION BY t.company_id, t.timestamp ORDER BY n.timestamp DESC) AS rn
-                FROM
-                    TradingData t
-                JOIN
-                    NewsCompanyLink ncl
-                ON
-                    t.company_id = ncl.company_id
-                JOIN
-                    News n
-                ON
-                    ncl.news_id = n.id
-                AND
-                    n.timestamp <= t.timestamp
-                AND 
-                    n.timestamp >= t.timestamp - {news_relative_age_threshold}
-            )"""
-        )
-    # Apply filters if necessary
-    where_clause = []
-    if company_id:
-        where_clause.append(f"t.company_id = {company_id}")
-    if min_timestamp:
-        # Adjust the minimum timestamp to allow aggregate functions to work correctly.
-        # Filter with original min_timestamp at the end
-        max_window = max(windows) if windows else 0
-        # 28800 is time the market is closed
-        adjusted_min_timestamp = (
-            min_timestamp - (max_window * 60 + 28800) if min_timestamp else None
-        )
-        where_clause.append(f"t.timestamp >= {adjusted_min_timestamp}")
-    if max_timestamp:
-        where_clause.append(f"t.timestamp <= {max_timestamp}")
-    CTEs = "WITH " + "\n".join(CTEs) + "\n" if CTEs else ""
-    columns = ",\n".join(columns)
-    ranked_news_joins = "\n".join(ranked_news_joins) if ranked_news_joins else ""
-    where_clause = "WHERE " + " AND ".join(where_clause) if where_clause else ""
-    query = f"""
-    {CTEs}
-    SELECT 
-        {columns}
-    FROM 
-        TradingData t
-    JOIN 
-        Company c
-    ON 
-        t.company_id = c.id
-    {ranked_news_joins}
-    {where_clause}
-    ORDER BY 
-        t.company_id, t.timestamp;
-    """
-    return query
+        # Get various statistics over each window
+        for window in windows:
+            if avg_close:
+                columns.append(
+                    f"AVG(t.close) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {window} PRECEDING AND CURRENT ROW) AS ma_{window}"
+                )
+            if avg_volume:
+                columns.append(
+                    f"AVG(t.volume) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {window} PRECEDING AND CURRENT ROW) AS avg_volume_{window}"
+                )
+            if std_dev:
+                columns.append(
+                    f"""SQRT(AVG(t.close * t.close) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {window} PRECEDING AND CURRENT ROW) - 
+                        (AVG(t.close) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {window} PRECEDING AND CURRENT ROW) * 
+                        AVG(t.close) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {window} PRECEDING AND CURRENT ROW))) AS volatility_{window}"""
+                )
+
+        # get n_news columns of the last n news articles before TradingData.timestamp
+        ranked_news_joins = []
+        for idx in range(1, n_news + 1):
+            columns.append(f"n{idx}.news_id AS news{idx}_id")
+            columns.append(f"n{idx}.timestamp AS news{idx}_timestamp")
+            ranked_news_joins.append(
+                f"LEFT JOIN RankedNews n{idx} ON t.company_id = n{idx}.company_id AND t.timestamp = n{idx}.trading_timestamp AND n{idx}.rn = {idx}"
+            )
+        if ranked_news_joins:
+            CTEs.append(
+                f"""
+                RankedNews AS (
+                    SELECT
+                        n.id AS news_id,
+                        n.timestamp,
+                        t.company_id,
+                        t.timestamp AS trading_timestamp,
+                        ROW_NUMBER() OVER (PARTITION BY t.company_id, t.timestamp ORDER BY n.timestamp DESC) AS rn
+                    FROM
+                        TradingData t
+                    JOIN
+                        NewsCompanyLink ncl
+                    ON
+                        t.company_id = ncl.company_id
+                    JOIN
+                        News n
+                    ON
+                        ncl.news_id = n.id
+                    AND
+                        n.timestamp <= t.timestamp
+                    AND 
+                        n.timestamp >= t.timestamp - {news_relative_age_threshold}
+                )"""
+            )
+        # Apply filters if necessary
+        where_clause = []
+        if company_id:
+            where_clause.append(f"t.company_id = {company_id}")
+        if min_timestamp:
+            # Adjust the minimum timestamp to allow aggregate functions to work correctly.
+            # Filter with original min_timestamp at the end
+            max_window = max(windows) if windows else 0
+            # 28800 is time the market is closed
+            adjusted_min_timestamp = (
+                min_timestamp - (max_window * 60 + 28800) if min_timestamp else None
+            )
+            where_clause.append(f"t.timestamp >= {adjusted_min_timestamp}")
+        if max_timestamp:
+            where_clause.append(f"t.timestamp <= {max_timestamp}")
+        CTEs = "WITH " + "\n".join(CTEs) + "\n" if CTEs else ""
+        columns = ",\n".join(columns)
+        ranked_news_joins = "\n".join(ranked_news_joins) if ranked_news_joins else ""
+        where_clause = "WHERE " + " AND ".join(where_clause) if where_clause else ""
+        query = f"""
+        {CTEs}
+        SELECT 
+            {columns}
+        FROM 
+            TradingData t
+        JOIN 
+            Company c
+        ON 
+            t.company_id = c.id
+        {ranked_news_joins}
+        {where_clause}
+        ORDER BY 
+            t.company_id, t.timestamp;
+        """
+        return query
 
 
 # TODO
