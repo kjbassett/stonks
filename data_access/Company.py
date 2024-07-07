565b8e2819780913cd6ba7f44ff0479f9d5f3b99
Ken <bassettkennethj@gmail.com>
2024-07-07 19:10:55 -0400

    Adjusted company and industry DAOs to work with industry data and schema

--- data_access/Company.py
+++ data_access/Company.py
@@ -1,7 +1,6 @@
 from async_lru import alru_cache
 
 from .base_dao import BaseDAO
-from .dao_manager import dao_manager
 
 
 class Company(BaseDAO):
@@ -10,7 +9,7 @@ class Company(BaseDAO):
 
     @alru_cache(maxsize=500)
     async def get_or_create_company_id(
-        self, symbol: str = None, name: str = None, industry: str = None
+        self, symbol: str = None, name: str = None, industry_id: str = None
     ):
         if not symbol and not name:
             raise ValueError("Please provide either a symbol or a name.")
@@ -22,15 +21,6 @@ class Company(BaseDAO):
 
         # if not found, create a new company if symbol is provided
         if company.empty:
-
-            # get industry id
-            if industry:
-                industry_id = await dao_manager.get_dao(
-                    "Industry"
-                ).get_or_create_industry(industry)
-            else:
-                industry_id = None
-
             if symbol:
                 await self.insert(
                     {"symbol": symbol, "name": name, "industry_id": industry_id}
