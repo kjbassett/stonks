99d124aaac88e8fa4f941bf702a39c5f56cedfa5
Ken <bassettkennethj@gmail.com>
2025-01-14 23:47:46 -0500

    Reworked the query in data aggregator to include. Several new features, most notably max_window and num_windows arguments

--- data_access/DataAggregator.py
+++ data_access/DataAggregator.py
@@ -1,3 +1,6 @@
+import time
+
+import numpy as np
 import pandas as pd
 
 from .base_dao import BaseDAO
@@ -10,175 +13,200 @@ class DataAggregator(BaseDAO):
 
     async def get_data(
         self,
-        company_id: int = None,
-        min_timestamp: int = None,
-        max_timestamp: int = None,
-        price_change_window: int = 86400,
-        avg_close: bool = True,
-        avg_volume: bool = True,
-        std_dev: bool = True,
-        windows: iter = None,
-        n_news: int = 3,
-        news_relative_age_threshold: int = 24 * 60 * 60,
+        min_timestamp: int = 0,
+        max_timestamp: int = 0,
+        max_window: int = 0,
+        num_windows: int = 0,
+        num_news: int = 0,
+        news_history_threshold: int = 24 * 60 * 60,
+        include_symbol: bool = False,
+        include_industry: bool = False,
+        include_office: bool = False,
+        include_price_change: bool = False,
+        include_volume_change: bool = False,
+        include_coeff_var: bool = False,
+        include_price_over_average: bool = False,
+        include_volume_over_average: bool = False,
     ) -> pd.DataFrame:
-        if windows is None:
-            windows = []
-        query = await self.construct_query(
-            company_id,
+        query = construct_query(
             min_timestamp,
             max_timestamp,
-            price_change_window,
-            avg_close,
-            avg_volume,
-            std_dev,
-            windows,
-            news_relative_age_threshold,
-            n_news,
+            max_window,
+            num_windows,
+            num_news,
+            news_history_threshold,
+            include_symbol,
+            include_industry,
+            include_office,
+            include_price_change,
+            include_volume_change,
+            include_coeff_var,
+            include_price_over_average,
+            include_volume_over_average,
         )
         data = await self.db.execute_query(
-            query, query_type="SELECT", return_type="DataFrame"
+            query, query_type="SELECT", return_type="DataFrame", print_query=True
         )
-
-        if min_timestamp:
-            data = data[data["timestamp"] >= min_timestamp]
-        if max_timestamp:
-            data = data[data["timestamp"] <= max_timestamp]
+        print(data.dtypes)
 
         return data
 
-    async def construct_query(
-        self,
-        company_id,
-        min_timestamp,
-        max_timestamp,
-        price_change_window,
-        avg_close,
-        avg_volume,
-        std_dev,
-        windows,
-        news_relative_age_threshold,
-        n_news,
-    ):
-        # Common table expressions
-        CTEs = []
-        columns = [
-            "t.company_id",
-            "t.timestamp",
-            "t.open",
-            "t.high",
-            "t.low",
-            "t.close",
-            "t.vw_average",
-            "t.volume",
-            "c.symbol",
-            f"""
-            (
-                (SELECT (
-                    SELECT t2.close 
-                    FROM TradingData t2 
-                    WHERE
-                        t2.company_id = t.company_id
-                        AND t2.timestamp >= t.timestamp + {price_change_window}  -- 900 seconds = 15 minutes
-                        AND t2.timestamp <= t.timestamp + {price_change_window} + 900  -- 900 seconds = 15 minutes
-                    ORDER BY t2.timestamp ASC
-                    LIMIT 1
-                ) - t.close) / t.close
-            ) AS target
-            """
-        ]
 
-        # one hot encode each industry
-        industry_groups = await self.db.execute_query(
-            "SELECT * FROM IndustryOffice;", return_type="DataFrame"
-        )
-        for i, row in industry_groups.iterrows():
-            name = row["name"].replace(" ", "_").replace("&", "and")
-            columns.append(
-                f"CASE WHEN (io.id = {row.id}) THEN 1 ELSE 0 END AS industry_{name}"
-            )
+def construct_query(
+    min_timestamp: int = 0,
+    max_timestamp: int = 0,
+    max_window: int = 0,
+    num_windows: int = 0,
+    num_news: int = 0,
+    news_history_threshold: int = 24 * 60 * 60,
+    include_symbol: bool = False,
+    include_industry: bool = False,
+    include_office: bool = False,
+    include_price_change: bool = False,
+    include_volume_change: bool = False,
+    include_coeff_var: bool = False,
+    include_price_over_average: bool = False,
+    include_volume_over_average: bool = False,
+) -> str:
+    # TODO break this function into smaller parts. One for each condition
+    # Initialize the columns for the query
+    ctes = []  # common table expressions
+    columns = ["t.close", "t.vw_average"]
+    joins = []
 
-        # Get various statistics over each window
-        for window in windows:
-            if avg_close:
-                columns.append(
-                    f"AVG(t.close) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {window} PRECEDING AND CURRENT ROW) AS ma_{window}"
-                )
-            if avg_volume:
-                columns.append(
-                    f"AVG(t.volume) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {window} PRECEDING AND CURRENT ROW) AS avg_volume_{window}"
-                )
-            if std_dev:
-                columns.append(
-                    f"""SQRT(AVG(t.close * t.close) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {window} PRECEDING AND CURRENT ROW) - 
-                        (AVG(t.close) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {window} PRECEDING AND CURRENT ROW) * 
-                        AVG(t.close) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {window} PRECEDING AND CURRENT ROW))) AS volatility_{window}"""
-                )
-
-        # get n_news columns of the last n news articles before TradingData.timestamp
-        ranked_news_joins = []
-        for idx in range(1, n_news + 1):
-            columns.append(f"n{idx}.news_id AS news{idx}_id")
-            columns.append(f"n{idx}.timestamp AS news{idx}_timestamp")
-            ranked_news_joins.append(
-                f"LEFT JOIN RankedNews n{idx} ON t.company_id = n{idx}.company_id AND t.timestamp = n{idx}.trading_timestamp AND n{idx}.rn = {idx}"
-            )
-        if ranked_news_joins:
-            CTEs.append(
-                f"""
-                RankedNews AS (
-                    SELECT
-                        n.id AS news_id,
-                        n.timestamp,
-                        t.company_id,
-                        t.timestamp AS trading_timestamp,
-                        ROW_NUMBER() OVER (PARTITION BY t.company_id, t.timestamp ORDER BY n.timestamp DESC) AS rn
-                    FROM TradingData t
-                    JOIN NewsCompanyLink ncl
-                    ON t.company_id = ncl.company_id
-                    JOIN News n
-                        ON ncl.news_id = n.id
-                        AND n.timestamp <= t.timestamp
-                        AND n.timestamp >= t.timestamp - {news_relative_age_threshold}
-                )"""
-            )
-        # Apply filters if necessary
-        where_clause = []
-        if company_id:
-            where_clause.append(f"t.company_id = {company_id}")
-        if min_timestamp:
-            # Adjust the minimum timestamp to allow aggregate functions to work correctly.
-            # Filter with original min_timestamp at the end
-            max_window = max(windows) if windows else 0
-            # 28800 is time the market is closed
-            adjusted_min_timestamp = (
-                min_timestamp - (max_window * 60 + 28800) if min_timestamp else None
-            )
-            where_clause.append(f"t.timestamp >= {adjusted_min_timestamp}")
-        if max_timestamp:
-            where_clause.append(f"t.timestamp <= {max_timestamp}")
-        CTEs = "WITH " + "\n".join(CTEs) + "\n" if CTEs else ""
-        columns = ",\n".join(columns)
-        ranked_news_joins = "\n".join(ranked_news_joins) if ranked_news_joins else ""
-        where_clause = "WHERE " + " AND ".join(where_clause) if where_clause else ""
-        query = f"""
-        {CTEs}
-        SELECT 
-            {columns}
-        FROM TradingData t
-        JOIN Company c
-            ON t.company_id = c.id
-        LEFT JOIN Industry i
-            ON c.industry_id = i.id
-        LEFT JOIN IndustryOffice io
-            ON i.office_id = io.id
-        {ranked_news_joins}
-        {where_clause}
-        ORDER BY 
-            t.company_id, t.timestamp;
+    # hour, day of week, and month of year
+    columns.append("strftime('%H', datetime(t.timestamp, 'unixepoch')) AS hour")
+    columns.append(
         """
-        return query
+    CASE strftime('%w', datetime(t.timestamp, 'unixepoch')) 
+        WHEN '0' THEN 'Sunday'
+        WHEN '1' THEN 'Monday'
+        WHEN '2' THEN 'Tuesday'
+        WHEN '3' THEN 'Wednesday'
+        WHEN '4' THEN 'Thursday'
+        WHEN '5' THEN 'Friday'
+        WHEN '6' THEN 'Saturday'
+    END AS day_of_week"""
+    )
+    columns.append(
+        """CASE strftime('%m', datetime(t.timestamp, 'unixepoch'))
+        WHEN '01' THEN 'January'
+        WHEN '02' THEN 'February'
+        WHEN '03' THEN 'March'
+        WHEN '04' THEN 'April'
+        WHEN '05' THEN 'May'
+        WHEN '06' THEN 'June'
+        WHEN '07' THEN 'July'
+        WHEN '08' THEN 'August'
+        WHEN '09' THEN 'September'
+        WHEN '10' THEN 'October'
+        WHEN '11' THEN 'November'
+        WHEN '12' THEN 'December'
+    END AS month_name"""
+    )
+
+    # company symbol
+    if include_symbol:
+        columns.append("c.symbol")
+        joins.append("JOIN Company c ON t.company_id = c.id")
+
+    # industry
+    if include_industry:
+        columns.append("i.name AS industry")
+        industry_join = "JOIN Industry i ON c.industry_id = i.id"
+        joins.append(industry_join)
+
+    # industry office (aka industry category)
+    if include_office:
+        columns.append("io.name AS office")
+        if industry_join not in joins:
+            joins.append(industry_join)
+        joins.append("JOIN IndustryOffice io ON i.office_id = io.id")
+
+    # news ids
+    if num_news > 0:
+        ctes.append(
+            f"""RankedNews AS (
+        SELECT
+            n.id AS news_id,
+            n.timestamp,
+            t.company_id,
+            t.timestamp AS trading_timestamp,
+            ROW_NUMBER() OVER (PARTITION BY t.company_id, t.timestamp ORDER BY n.timestamp DESC) AS rn
+        FROM TradingData t
+        JOIN NewsCompanyLink ncl ON t.company_id = ncl.company_id
+        JOIN News n ON ncl.news_id = n.id
+        WHERE n.timestamp <= t.timestamp -- TODO might want to simulate the time between news and trading data irl
+        AND n.timestamp >= t.timestamp - {news_history_threshold}
+    )"""
+        )
+        for i in range(1, num_news + 1):
+            columns.append(f"n{i}.news_id AS news{i}_id")
+            joins.append(
+                f"LEFT JOIN RankedNews n{i} ON t.company_id = n{i}.company_id AND t.timestamp = n{i}.trading_timestamp AND n{i}.rn = {i}"
+            )
+
+    # Add statistics for each window
+    if max_window > 0 and num_windows > 0:
+        # TODO explore non-overlapping windows going back into the past.
+        window_sizes = np.linspace(0, max_window, num_windows + 1, dtype=int)[1:]
+        window_sizes = set(window_sizes)  # ensure we don't have duplicate window sizes
+        for offset in window_sizes:
+            # current price / past price
+            if include_price_change:
+                columns.append(
+                    f"(t.close / LAG(t.close, {offset}) OVER (PARTITION BY t.company_id ORDER BY t.timestamp)) AS price_ratio_window_{offset}"
+                )
+            # current volume / past volume
+            if include_volume_change:
+                columns.append(
+                    f"(t.volume / LAG(t.volume, {offset}) OVER (PARTITION BY t.company_id ORDER BY t.timestamp)) AS volume_ratio_window_{offset}"
+                )
+            # coefficient of variation of price over window
+            if include_coeff_var:
+                columns.append(
+                    f"""
+    SQRT(
+        AVG(t.close * t.close) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {offset} PRECEDING AND CURRENT ROW) - 
+        (
+            AVG(t.close) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {offset} PRECEDING AND CURRENT ROW) * 
+            AVG(t.close) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {offset} PRECEDING AND CURRENT ROW)
+        )
+    ) / AVG(t.close) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {offset} PRECEDING AND CURRENT ROW) as coeff_var_window_{offset}"""
+                )
+            # current price / average price over window
+            if include_price_over_average:
+                columns.append(
+                    f"(t.close / AVG(t.close) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {offset} PRECEDING AND CURRENT ROW)) AS price_avg_ratio_window_{offset}"
+                )
+            # current volume / average volume over window
+            if (
+                include_volume_over_average
+            ):  # current volume / average volume over window
+                columns.append(
+                    f"(t.volume / AVG(t.volume) OVER (PARTITION BY t.company_id ORDER BY t.timestamp ROWS BETWEEN {offset} PRECEDING AND CURRENT ROW)) AS volume_avg_ratio_window_{offset}"
+                )
+
+    # format query parts
+    ctes = "WITH " + ",\n".join(ctes) + "\n" if ctes else ""
+    columns = ",\n".join(columns)
+    joins = "\n".join(joins)
+
+    # Construct the query
+    query = f"""
+{ctes} 
+SELECT {columns}
+FROM TradingData t
+{joins}
+WHERE t.timestamp >= {min_timestamp} and t.timestamp <= {max_timestamp if max_timestamp else time.time()}
+ORDER BY t.timestamp
+"""
+
+    return query
 
 
 # TODO
+#  Cast hour as int
+#  Get diff of current timestamp and news timestamps
+#  Verify hour is correct (and day of week and month with same fix if needed). Just put timestamp into data and convert it online.
 #  How to tokenize company in text?
-#  Data Generator
