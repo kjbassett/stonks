f17f96df2930ab9519db295e63a433d617dcdfe4
Ken <bassettkennethj@gmail.com>
2025-01-15 20:57:00 -0500

    Switched to a smaller encoder + tokenizer and lowered batch size to reduce memory usage. Implemented get_random_batch in DataGenerator

--- plugins/prediction/data_generator.py
+++ plugins/prediction/data_generator.py
@@ -13,7 +13,7 @@ class DataGenerator:
         self.batch_size = batch_size
         self.max_text_length = max_text_length
         self.shuffle = shuffle
-        self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+        self.tokenizer = BertTokenizer.from_pretrained("M-FAC/bert-tiny-finetuned-mrpc")
         if shuffle_data:
             self.data = shuffle(self.data)
 
@@ -43,21 +43,35 @@ class DataGenerator:
         return result
 
     async def get_batch(self, index):
-        batch_data = self.data[index * self.batch_size: (index + 1) * self.batch_size]
+        index = index % len(self)
+        batch_data = self.data[index * self.batch_size : (index + 1) * self.batch_size]
+        # for debugging. delete me later
+        if len(batch_data.index) < 50 and index == 0:
+            batch_data.to_csv(f"batch{index}_data.csv")
+        y = batch_data["target"].values
+        x = await self.get_news(batch_data)
+        return x, y
+
+    async def get_news(self, batch_data):
         encoded_text = []
-
         news_columns = _get_news_columns(batch_data)
-
         news_texts_list = await fetch_all_news(batch_data, news_columns)
         for news_texts in news_texts_list:
             encoded_text.append(self.encode_texts(news_texts))
 
-        # for debugging. delete me later
-        if len(batch_data.index) < 50:
-            batch_data.to_csv(f"batch{index}_data.csv")
         structured_data = batch_data.drop(columns=news_columns + ["target"]).values
-        x = np.hstack([encoded_text, structured_data])
+        try:
+            x = np.hstack([encoded_text, structured_data])
+        except ValueError as e:
+            print(f"Error occurred while fetching news")
+            raise e
+        return x
+
+    async def get_random_batch(self):
+        indices = np.random.choice(len(self), self.batch_size, replace=False)
+        batch_data = self.data.iloc[indices]
         y = batch_data["target"].values
+        x = await self.get_news(batch_data)
         return x, y
 
 
@@ -72,7 +86,10 @@ async def fetch_all_news(batch_data, news_columns):
     return await asyncio.gather(*tasks)
 
 
-async def fetch_news(news_ids):
+async def fetch_news(news_ids: list):
+    """
+    Fetches news data for a give list of news_ids
+    """
     news_dao = dao_manager.get_dao("News")
     news_texts = []
     for news_id in news_ids:
@@ -126,17 +143,14 @@ async def create_generators(
         n_news,
         news_relative_age_threshold,
     )
-    data = data.drop(columns=['company_id', 'timestamp', 'symbol'])
     data = shuffle(data)
     n_train = int(0.8 * len(data))
     if batch_size == 0:
         batch_size = n_train
-    train = data.loc[: n_train]
+    train = data.loc[:n_train]
     test = data.loc[n_train:]
     train_generator = DataGenerator(
         train, batch_size=batch_size, max_text_length=max_text_length, shuffle_data=True
     )
-    test_generator = DataGenerator(
-        test, batch_size=len(test.index), max_text_length=max_text_length
-    )
+    test_generator = DataGenerator(test, batch_size=15, max_text_length=max_text_length)
     return train_generator, test_generator
