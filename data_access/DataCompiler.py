a979c504350828581a68280a1974e51c5298f0c1
Ken <bassettkennethj@gmail.com>
2025-04-18 09:27:14 -0400

    WIP Original aggregation code is now in DataCompiler. In the process of making it work for different aggregation intervals

--- data_access/DataCompiler.py
+++ data_access/DataCompiler.py
@@ -20,8 +20,6 @@ class DataCompiler(BaseDAO):
         num_windows: int = 0,
         num_news: int = 0,
         news_history_threshold: int = 24 * 60 * 60,
-        include_industry: bool = False,
-        include_office: bool = False,
         include_price_change: bool = False,
         include_volume_change: bool = False,
         include_coeff_var: bool = False,
@@ -37,8 +35,6 @@ class DataCompiler(BaseDAO):
             num_windows,
             num_news,
             news_history_threshold,
-            include_industry,
-            include_office,
             include_price_change,
             include_volume_change,
             include_coeff_var,
@@ -54,111 +50,47 @@ class DataCompiler(BaseDAO):
 
 
 def construct_query(
-    price_change_offset: int = 86400,  # 15 minutes in seconds
+    aggregation_interval: str,
+    price_change_offset: int = 86400 * 5,  # 1 day in seconds x 5
     min_timestamp: int = 0,
     max_timestamp: int = 0,
     max_window: int = 0,
     num_windows: int = 0,
     num_news: int = 0,
-    news_history_threshold: int = 24 * 60 * 60,
-    include_industry: bool = False,
-    include_office: bool = False,
+    news_history_threshold: int = 86400,
     include_price_change: bool = False,
     include_volume_change: bool = False,
     include_coeff_var: bool = False,
     include_price_over_average: bool = False,
     include_volume_over_average: bool = False,
 ) -> str:
-    # TODO break this function into smaller parts. One for each condition
-    # Initialize the columns for the query
     ctes = []  # common table expressions
-    columns = ["t.close", "t.vw_average", "c.symbol", "c.name"]
-    joins = ["JOIN Company c ON t.company_id = c.id"]
+    columns = []
+    joines = []
+    if aggregation_interval == "minute":
+        columns += ["t.close", "t.vw_average", 'i.name', 'io.name']
+    elif aggregation_interval == "hour":
+        columns += ['t.open', 't.low', 't.close', 't.avg_close', 't.cv_close',
+                    't.price_change', 't.avg_volume', 't.cv_volume', 't.row_count', 'i.name', 'io.name']
+    joins += ["JOIN Company c ON t.company_id = c.id",
+              "JOIN Industry i ON c.industry_id = i.id",
+              "JOIN IndustryOffice io ON i.office_id = io.id"]
+
 
     # target column
-    pco_min = price_change_offset - 0.02 * price_change_offset
-    pco_max = price_change_offset + 0.02 * price_change_offset
-    columns.append(f"""
-CAST(((
-    SELECT AVG(t2.close)
-    FROM TradingData t2 
-    WHERE
-        t2.company_id = t.company_id
-        AND t2.timestamp >= t.timestamp + {pco_min}
-        AND t2.timestamp <= t.timestamp + {pco_max}
-) - t.close) / t.close AS REAL) AS target""")
+    columns.append(construct_target_column(aggregation_interval, price_change_offset))
 
     # hour, day of week, and month of year
-    columns.append("CAST(strftime('%H', datetime(t.timestamp, 'unixepoch')) AS INTEGER) AS hour")
-    columns.append(
-        """
-    CASE strftime('%w', datetime(t.timestamp, 'unixepoch')) 
-        WHEN '0' THEN 'Sunday'
-        WHEN '1' THEN 'Monday'
-        WHEN '2' THEN 'Tuesday'
-        WHEN '3' THEN 'Wednesday'
-        WHEN '4' THEN 'Thursday'
-        WHEN '5' THEN 'Friday'
-        WHEN '6' THEN 'Saturday'
-    END AS day_of_week"""
-    )
-    columns.append(
-        """CASE strftime('%m', datetime(t.timestamp, 'unixepoch'))
-        WHEN '01' THEN 'January'
-        WHEN '02' THEN 'February'
-        WHEN '03' THEN 'March'
-        WHEN '04' THEN 'April'
-        WHEN '05' THEN 'May'
-        WHEN '06' THEN 'June'
-        WHEN '07' THEN 'July'
-        WHEN '08' THEN 'August'
-        WHEN '09' THEN 'September'
-        WHEN '10' THEN 'October'
-        WHEN '11' THEN 'November'
-        WHEN '12' THEN 'December'
-    END AS month_name"""
-    )
+    columns += construct_dt_columns(aggregation_interval)
 
-    # industry
-    # before "if include_industry" to guarantee that it gets defined for "if include_office" code block
-    industry_join = "JOIN Industry i ON c.industry_id = i.id"
-    if include_industry:
-        columns.append("i.name AS industry")
-        joins.append(industry_join)
-
-    # industry office (aka industry category)
-    if include_office:
-        columns.append("io.name AS office")
-        if industry_join not in joins:
-            joins.append(industry_join)
-        joins.append("JOIN IndustryOffice io ON i.office_id = io.id")
-
-    # news ids
-    if num_news > 0:
-        ctes.append(
-            f"""RankedNews AS (
-        SELECT
-            n.id AS news_id,
-            n.timestamp,
-            t.company_id,
-            t.timestamp AS trading_timestamp,
-            ROW_NUMBER() OVER (PARTITION BY t.company_id, t.timestamp ORDER BY n.timestamp DESC) AS rn
-        FROM TradingData t
-        JOIN NewsCompanyLink ncl ON t.company_id = ncl.company_id
-        JOIN News n ON ncl.news_id = n.id
-        WHERE n.timestamp <= t.timestamp -- TODO might want to simulate the time between news and trading data irl
-        AND n.timestamp >= t.timestamp - {news_history_threshold}
-    )"""
-        )
-        for i in range(1, num_news + 1):
-            columns.append(f"n{i}.news_id AS news{i}_id")
-            joins.append(
-                f"LEFT JOIN RankedNews n{i} ON t.company_id = n{i}.company_id AND t.timestamp = n{i}.trading_timestamp AND n{i}.rn = {i}"
-            )
+     # news ids
+    news_cte, news_cols, news_joins = construct_news_columns(aggregation_interval, num_news)
+    ctes.append(news_cte)
+    columns += news_cols
+    joins += news_joins
 
     # Add statistics for each window
     if max_window > 0 and num_windows > 0:
-        # TODO explore non-overlapping windows going back into the past.
         window_sizes = np.linspace(0, max_window, num_windows + 1, dtype=int)[1:]
         window_sizes = set(window_sizes)  # ensure we don't have duplicate window sizes
         for offset in window_sizes:
@@ -206,7 +138,7 @@ CAST(((
     query = f"""
 {ctes} 
 SELECT {columns}
-FROM TradingData t
+FROM TradingData{"Aggregations" if aggregation_interval != "minute" else ""} t
 {joins}
 WHERE t.timestamp >= {min_timestamp} and t.timestamp <= {max_timestamp if max_timestamp else time.time()}
 ORDER BY t.timestamp
@@ -214,6 +146,122 @@ ORDER BY t.timestamp
 
     return query
 
+
+def construct_target_column(aggregation_interval, price_change_offset):
+    if aggregation_interval == "minute":
+        # TODO Does not account for hours that the market isn't open
+        # target column
+        pco_min = price_change_offset - 0.02 * price_change_offset
+        pco_max = price_change_offset + 0.02 * price_change_offset
+        return f"""
+CAST(((
+    SELECT AVG(t2.close)
+    FROM TradingData t2 
+    WHERE
+        t2.company_id = t.company_id
+        AND t2.timestamp >= t.timestamp + {pco_min}
+        AND t2.timestamp <= t.timestamp + {pco_max}
+) - t.close) / t.close AS REAL) AS target"""
+    elif aggregation_interval == "hour":
+        # Grab the price_change_offset-th row after the current row
+        return f"""
+CAST(((
+    SELECT AVG(t2.close)
+    FROM (
+        SELECT t2.close
+        FROM TradingDataAggregation t2
+        WHERE
+            t2.company_id = t.company_id
+            AND t2.interval = 'hourly'
+            AND (t2.date > t.date OR (t2.date = t.date AND t2.hour > t.hour))
+        ORDER BY t2.date, t2.hour
+        LIMIT 1 OFFSET {price_change_offset - 1}
+    )
+) - t.close) / t.close AS REAL) AS target"""
+
+def construct_dt_columns(aggregation_interval):
+    columns = []
+    weekday_str = """
+    WHEN '0' THEN 'Sunday'
+    WHEN '1' THEN 'Monday'
+    WHEN '2' THEN 'Tuesday'
+    WHEN '3' THEN 'Wednesday'
+    WHEN '4' THEN 'Thursday'
+    WHEN '5' THEN 'Friday'
+    WHEN '6' THEN 'Saturday'
+    END AS day_of_week
+    """
+    month_str = """
+    WHEN '01' THEN 'January'
+    WHEN '02' THEN 'February'
+    WHEN '03' THEN 'March'
+    WHEN '04' THEN 'April'
+    WHEN '05' THEN 'May'
+    WHEN '06' THEN 'June'
+    WHEN '07' THEN 'July'
+    WHEN '08' THEN 'August'
+    WHEN '09' THEN 'September'
+    WHEN '10' THEN 'October'
+    WHEN '11' THEN 'November'
+    WHEN '12' THEN 'December'
+    END AS month_name"""
+
+    # Add hour and weekday name, if hourly, we already have date and hour in TradingDataAggregation
+    if aggregation_interval == "minute":
+        columns.append("CAST(strftime('%H', datetime(t.timestamp, 'unixepoch')) AS INTEGER) AS hour")
+        columns.append(f"CASE strftime('%w', datetime(t.timestamp, 'unixepoch')) {weekday_str}")
+        columns.append(f"CASE strftime('%m', datetime(t.timestamp, 'unixepoch')) {month_str}")
+    elif aggregation_interval == "hour":
+        columns.append("t.hour")
+        columns.append(f"CASE strftime('%w', t.date) {weekday_str}")
+        columns.append(f"CASE strftime('%m', t.date) {month_str}")
+
+
+def construct_news_columns(aggregation_interval, num_news, news_history_threshold, get_ids=False):
+    if num_news < 1:
+        return "", [], []
+
+    if aggregation_interval == "minute":
+        t_col = "t.timestamp"
+        table = "TradingData"
+    elif aggregation_interval == "hour":
+        t_col = "t.end"
+        table = "TradingDataAggregation"
+    else:
+        raise ValueError("Unsupported aggregation interval")
+
+    if get_ids:
+        formula = "n.id"
+        data_col = "news_id"
+    elif not get_ids:
+        formula = "CONCAT(t.symbol, ' ', t.name, ' ', n.body)"
+        data_col = "news_text"
+    else:
+        raise ValueError("Unsupported value for variable 'get_ids'")
+
+    cte = f"""
+    RankedNews AS (
+    SELECT
+        {formula} AS {data_col},
+        n.timestamp,
+        t.company_id,
+        {t_col} AS trade_ts,
+        ROW_NUMBER() OVER (PARTITION BY t.company_id, t.timestamp ORDER BY n.timestamp DESC) AS rn
+    FROM {table} t
+    JOIN NewsCompanyLink ncl ON t.company_id = ncl.company_id
+    JOIN News n ON ncl.news_id = n.id
+    WHERE n.timestamp <= {t_col} -- TODO might want to simulate the time between news and trading data irl
+    AND n.timestamp >= {t_col} - {news_history_threshold}
+    )"""
+    columns = []
+    joins = []
+    for i in range(1, num_news + 1):
+        columns.append(f"n{i}.news_id AS news{i}_id")
+        joins.append(
+            f"LEFT JOIN RankedNews n{i} ON t.company_id = n{i}.company_id AND {t_col} = n{i}.trade_ts AND n{i}.rn = {i}"
+        )
+
+    return cte, columns, joins
 
 # TODO
 #  Get diff of current timestamp and news timestamps
