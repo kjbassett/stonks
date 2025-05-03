6a6eb548f92126ba8865bcdd89ebf2045140df9a
Ken <bassettkennethj@gmail.com>
2025-05-02 23:03:47 -0400

    Added ability to aggregate one section at a time instead of all at once. Created placeholder plugin to execute aggregation

--- data_access/TradingDataAggregation.py
+++ data_access/TradingDataAggregation.py
@@ -1,20 +1,44 @@
+from datetime import datetime, time
+
+from config import CONFIG
+
 from .base_dao import BaseDAO
 from .db.async_database import AsyncDatabase
 
 
 class TradingDataAggregation(BaseDAO):
     def __init__(self, db: AsyncDatabase):
-        super().__init__(db, "TradingData")
+        super().__init__(db, "TradingDataAggregation")
 
-    async def update_missing_hourly_aggregations(self):
-        import time
-        t = time.time()
-        query = """
-WITH LatestTimestamps AS (
-    SELECT company_id, MAX(timestamp) AS latest_timestamp
-    FROM TradingData
-    GROUP BY company_id
-),
+    async def update_missing_hourly_aggregations(self, window: int = 3600 * 24):
+        earliest_timestamp = int(
+            datetime.combine(CONFIG["min_date"], time()).timestamp()
+        )  # midnight of earliest date in seconds since epoch
+        now = int(datetime.now().timestamp())
+        for t in range(earliest_timestamp, now, window):
+            if (
+                t + window > now
+            ):  # final iteration, don't aggregate past the current time
+                latest_ts_cte = """
+                -- Get the latest timestamp for each company. Used later to make sure we don't aggregate an hour of incomplete data.
+                LatestTimestamps AS ( 
+                    SELECT company_id, MAX(timestamp) AS latest_timestamp
+                    FROM TradingData
+                    GROUP BY company_id
+                ),"""
+                grouped_data_part = f"""
+                    JOIN LatestTimestamps lt ON fd.company_id = lt.company_id
+                    GROUP BY fd.company_id, fd.date, fd.hour
+                    HAVING period_end <= lt.latest_timestamp
+                """
+            else:
+                latest_ts_cte = ""
+                grouped_data_part = "GROUP BY fd.company_id, fd.date, fd.hour"
+
+            query = f"""
+WITH
+    {latest_ts_cte}
+-- Get trading data that has not been aggregated yet.
 FilteredData AS (
     SELECT
         td.company_id,
@@ -31,8 +55,9 @@ FilteredData AS (
     ON td.company_id = tda.company_id
     AND DATE(td.timestamp, 'unixepoch') = tda.date
     AND strftime('%H', td.timestamp, 'unixepoch') = tda.hour
-    WHERE tda.company_id IS NULL
+    WHERE tda.company_id IS NULL AND td.timestamp >= {t} AND td.timestamp < {t + window}
 ),
+-- Group the filtered data by company, date, and hour.
 GroupedData AS (
     SELECT
         fd.company_id,
@@ -51,10 +76,9 @@ GroupedData AS (
         COUNT(*) AS row_count,
         CAST(strftime('%s', fd.date || ' ' || fd.hour || ':59:59') AS INTEGER) AS period_end
     FROM FilteredData fd
-    JOIN LatestTimestamps lt ON fd.company_id = lt.company_id
-    GROUP BY fd.company_id, fd.date, fd.hour
-    HAVING period_end <= lt.latest_timestamp
+    {grouped_data_part}
 ),
+-- Perform some additional calculations from the grouped data
 CalculatedMetrics AS (
     SELECT
         gd.company_id,
@@ -78,6 +102,7 @@ CalculatedMetrics AS (
         ELSE 0 END) AS cv_volume
     FROM GroupedData gd
 )
+-- Finally, insert the calculated metrics into the TradingDataAggregation table.
 INSERT INTO TradingDataAggregation (
     company_id, interval, date, hour, start, end, open, high, low, close, avg_close, cv_close, price_change, avg_volume, cv_volume, row_count
 )
@@ -85,6 +110,10 @@ SELECT
     company_id, 'hourly', date, hour, start, end, open, high, low, close, avg_close, cv_close, price_change, avg_volume, cv_volume, row_count
 FROM CalculatedMetrics;
         """
-        await self.db.execute_query(query)
-        print(f"Time to group data by hour: {time.time() - t} seconds")
-        print("Missing hourly aggregations updated.")
+            current_iter_start = datetime.now().timestamp()
+            print(f"Starting aggegation query from {t} to {t + window}, end at {now}")
+            await self.db.execute_query(query)
+            print(
+                f"Time to group data by hour: {int(datetime.now().timestamp() - current_iter_start)} seconds"
+            )
+            print("Missing hourly aggregations updated.")
