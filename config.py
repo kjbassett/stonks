83629a694ec3db6edc7f39f7db08cdaec5098fea
Ken <bassettkennethj@gmail.com>
2025-05-18 20:26:44 -0400

    Removed config.py from source control

--- config.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import os
-
-CONFIG = {
-    'root_path': os.getcwd(),
-    'min_date': datetime.date.today() - datetime.timedelta(days=730),
-    'db_folder': 'H:\databases',
-    'db_name': 'stonksDB.sqlite'
-}
