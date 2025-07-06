0485691e753aaf7fc308a1a6b2d62f5252d01be2
Ken <bassettkennethj@gmail.com>
2025-07-05 20:03:01 -0400

    deleted two more files from webrock

--- plugins/decorator.py
+++
@@ -1,7 +0,0 @@
-def plugin(**decorator_metadata):
-    def decorator(func):
-        func.is_plugin = True
-        func.decorator_metadata = decorator_metadata
-        return func
-
-    return decorator
