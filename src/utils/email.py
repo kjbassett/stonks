d1fdc2d85e824e9846dc467d260a0aac4da2d1d5
Ken <bassettkennethj@gmail.com>
2025-11-15 18:09:33 -0500

    added email function to utilities.py

---
+++ src/utils/email.py
@@ -0,0 +1,18 @@
+import smtplib
+from email.mime.text import MIMEText
+
+from config import CONFIG
+from webrock.decorator import plugin
+
+
+@plugin()
+def send_email(subject: str, body: str, recipient: str):
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = CONFIG["gmail_address"]
+    msg["To"] = recipient
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp_server:
+        smtp_server.login(CONFIG["gmail_address"], CONFIG["gmail_pw"])
+        smtp_server.sendmail(CONFIG["gmail_address"], recipient, msg.as_string())
+    print("Message sent!")
